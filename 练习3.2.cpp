f2dbe1a5722725823cf3819efbb9aee20baa1f14
OnePointFive99 <lzystan2021@163.com>
2023-10-20 14:41:09 +0800

    更新练习3.2 3.4

--- 练习3.2.cpp
+++ 练习3.2.cpp
@@ -11,6 +11,23 @@ int main() {
     return 0;
 }
 
+答案：
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+
+int main()
+{
+    for (string str; getline(cin, str); cout << str << endl);
+    return 0;
+}
+
+
 #include <iostream>
 using std::cin;
 using std::cout;
@@ -21,5 +38,20 @@ int main() {
     cin>>str;
     cout<<str<<endl;
 
+    return 0;
+}
+
+答案：
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    for (string str; cin >> str; cout << str << endl);
     return 0;
 }
--- 练习3.4.cpp
+++ 练习3.4.cpp
@@ -21,6 +21,27 @@ int main() {
 
     return 0;
 }
+答案：
+#include <iostream>
+#include <string>
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    string str1, str2;
+    while (cin >> str1 >> str2)
+    {
+        if (str1 == str2)
+            cout << "The two strings are equal." << endl;
+        else
+            cout << "The larger string is " << ((str1 > str2) ? str1 : str2);
+    }
+
+    return 0;
+}
 
 
 #include <iostream>
@@ -42,6 +63,28 @@ int main() {
             cout<<str2<<endl;
         }
         
+    }
+
+    return 0;
+}
+
+答案：
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    for (string str1, str2; cin >> str1 >> str2;/* */)
+    {
+        if (str1.size() == str2.size())
+            cout << "The two strings have the same length." << endl;
+        else
+            cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2) << endl;
     }
 
     return 0;
